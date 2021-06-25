d032857d99f5f1947ecc9919272561c956962e41
VISHARAD <47363928+VISHARAD17@users.noreply.github.com>
2021-06-25 21:09:53 +0530

    Add files via upload

---
+++ weird_algo.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double n;
+    cin >> n;
+    cout << setprecision(0) << fixed << n << " ";
+    
+    while(n != 1){
+        if(fmod(n,2) == 0){
+            n = n/2;
+            if(n == 1){
+                cout << 1;
+            }
+            else{
+                cout << setprecision(0)<< fixed << n <<" ";}
+        }
+        else {
+            n = 3*n + 1;
+            cout << setprecision(0)<< fixed << n << " ";
+        }
+    }
+    return 0;
+}
