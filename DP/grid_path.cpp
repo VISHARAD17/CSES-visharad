e63c040337cb1db17918705f0bc22bbb91840542
Visharad <visharadborsutkar777@gmail.com>
2021-10-18 21:19:12 +0530

    solved grid_paths from dynamic programming section

--- DP/grid_path.cpp
+++ DP/grid_path.cpp
@@ -1,46 +1,25 @@
 #include<bits/stdc++.h>
-#define mod 100000007;
 using namespace std;
+const int mod = 1e9+7;
 
 int main(){
     int n;
     cin >> n;
 
-    vector<vector<int>> arr(n, vector<int>(n, 0));
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            char ch;
-            cin >> ch;
-            arr[i][j] = (ch == '.') ? 1 : 0;
-        }
-    }
+    vector<string>grid(n);
+    for(int i=0; i<n; i++) cin >> grid[i];
 
-    vector<vector<int>> dp(n, vector<int>(n, 0));
-    if(arr[n-1][n-1] == 1)
-        dp[n - 1][n - 1] = 1;
+    vector<vector<int>>dp(n, vector<int>(n, 0));
+    dp[0][0] = (grid[0][0] == '.');
 
-    for (int i = n - 2; i >= 0; i--){
-        if(arr[i][n-1] == 0)
-            dp[i][n - 1] = 0;
-        else dp[i][n - 1] += dp[i + 1][n - 1];
-    }
-
-    for (int i = n-2; i >= 0; i--){
-        if(arr[n-1][i] == 0)
-            dp[n - 1][i] == 0;
-        else
-            dp[n - 1][i] += dp[n - 1][i + 1];
-    }
-
-    for (int i = n - 2; i >= 0; i--){
-        for (int j = n - 2; j >= 0; j--){
-            if(arr[i][j] == 0)
-                dp[i][j] = 0;
-            else {
-                dp[i][j] += (dp[i + 1][j] + dp[i][j + 1])% mod;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(grid[i][j] != '*') // if not an abstacle then proceed
+            {
+                if(i-1 >= 0 && grid[i-1][j] != '*') dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod; // from top if exists
+                if(j-1>=0 && grid[i][j-1] != '*') dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod; // from left if exists
             }
         }
     }
-    cout << dp[0][0];
-    return 0;
+    cout << dp[n-1][n-1];
 }
