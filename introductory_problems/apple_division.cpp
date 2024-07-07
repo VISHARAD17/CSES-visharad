3a73aea87b9f8acce0a1f42a611e68f8f7ee1e7b
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-07-07 15:40:01 +0530

    ununsed imports

--- introductory_problems/apple_division.cpp
+++ introductory_problems/apple_division.cpp
@@ -1,23 +1,10 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
-#include <complex>
-#include <queue>
-#include <set>
-#include <unordered_set>
-#include <list>
-#include <chrono>
-#include <random>
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#include <string>
 #include <vector>
-#include <map>
-#include <unordered_map>
-#include <stack>
-#include <iomanip>
-#include <fstream>
  
 using namespace std;
  
